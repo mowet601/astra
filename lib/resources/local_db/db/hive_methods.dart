5937b532fad4f838f223962bbdf5d268ad600972
Rivaan Ranawat <namanrivaan@gmail.com>
2020-09-24 23:31:05 +0530

    defining methods of sql and hive

--- lib/resources/local_db/db/hive_methods.dart
+++ lib/resources/local_db/db/hive_methods.dart
@@ -1,34 +1,62 @@
+import 'dart:io';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:astra/models/log.dart';
 import 'package:astra/resources/local_db/interface/log_interface.dart';
 
 class HiveMethods implements LogInterface {
+  String hive_box = "Call_Logs";
+
   @override
-  addLogs(Log log) {
-    print("Adding values to hive db");
-    return null;
+  init() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    Hive.init(dir.path);
   }
 
   @override
-  close() {
-    // TODO: implement close
-    return null;
+  addLogs(Log log) async {
+    var box = await Hive.openBox(hive_box);
+
+    var logMap = log.toMap(log);
+
+    int idOfInput = await box.add(logMap);
+
+    close();
+
+    return idOfInput;
+  }
+
+  updateLogs(int i, Log newLog) async {
+    // covers the check of the box being already open
+    var box = await Hive.openBox(hive_box);
+
+    var newLogMap = newLog.toMap(newLog);
+
+    box.putAt(i, newLogMap);
+
+    close();
   }
 
   @override
-  deleteLogs(int logId) {
-    // TODO: implement deleteLogs
-    return null;
+  Future<List<Log>> getLogs() async {
+    var box = await Hive.openBox(hive_box);
+
+    List<Log> logList = [];
+
+    for (int i = 0; i < box.length; i++) {
+      var logMap = box.getAt(i);
+      logList.add(Log.fromMap(logMap));
+    }
+    return logList;
   }
 
   @override
-  Future<List<Log>> getLogs() {
-    // TODO: implement getLogs
-    return null;
+  deleteLogs(int logId) async {
+    var box = await Hive.openBox(hive_box);
+
+    await box.deleteAt(logId);
   }
 
   @override
-  init() {
-    print("initialized hive db");
-    return null;
-  }
+  close() => Hive.close();
 }
--- lib/resources/local_db/db/sqlite_methods.dart
+++ lib/resources/local_db/db/sqlite_methods.dart
@@ -1,34 +1,112 @@
+import 'dart:io';
 import 'package:astra/models/log.dart';
 import 'package:astra/resources/local_db/interface/log_interface.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import "package:path/path.dart";
 
 class SqliteMethods implements LogInterface {
-  @override
-  addLogs(Log log) {
-    print("Adding values to Sqlite Db");
-    return null;
+  Database _db;
+  String databaseName =
+      "LogDB"; // change it dynamically in future for each user
+  String tableName = "Call_Logs";
+
+  // COLUMNS
+  String id = "log_id";
+  String callerName = "caller_name";
+  String callerPic = "caller_pic";
+  String receiverName = "receiver_name";
+  String receiverPic = "receiver_pic";
+  String callStatus = "call_status";
+  String timestamp = "timestamp";
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    print("db was null, now awaiting it");
+    _db = await init();
+    return _db;
   }
 
   @override
-  close() {
-    // TODO: implement close
-    return null;
+  init() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    String path =
+        join(dir.path, databaseName); // joins using '/' and makes it accessible
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return db;
+  }
+
+  _onCreate(Database db, int version) async {
+    // Columns database should have
+    String createTableQuery =
+        "CREATE TABLE $tableName ($id INTEGER PRIMARY KEY, $callerName TEXT, $callerPic TEXT, $receiverName TEXT, $receiverPic TEXT, $callStatus TEXT, $timestamp TEXT)";
+
+    await db.execute(createTableQuery);
+    print("table created!");
   }
 
   @override
-  deleteLogs(int logId) {
-    // TODO: implement deleteLogs
-    return null;
+  addLogs(Log log) async {
+    var dbClient = await db;
+    await dbClient.insert(tableName, log.toMap(log));
   }
 
   @override
-  Future<List<Log>> getLogs() {
-    // TODO: implement getLogs
-    return null;
+  deleteLogs(int logId) async {
+    var dbClient = await db;
+    return await dbClient
+        .delete(tableName, where: '$id = ?', whereArgs: [logId]);
+  }
+
+  updateLogs(Log log) async {
+    var dbClient = await db;
+    await dbClient.update(
+      tableName,
+      log.toMap(log),
+      where: '$id = ?',
+      whereArgs: [log.logId],
+    );
   }
 
   @override
-  init() {
-    print("Initialized sqlite db");
-    return null;
+  Future<List<Log>> getLogs() async {
+    try {
+      var dbClient = await db;
+
+      List<Map> maps = await dbClient.query(
+        tableName,
+        columns: [
+          id,
+          callerName,
+          callerPic,
+          receiverName,
+          receiverPic,
+          callStatus,
+          timestamp,
+        ],
+      );
+
+      List<Log> logList = [];
+
+      if (maps.isNotEmpty) {
+        for (Map map in maps) {
+          logList.add(Log.fromMap(map));
+        }
+      }
+
+      return logList;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  // not necessary -> db does it for us
+  @override
+  close() async {
+    var dbClient = await db;
+    dbClient.close();
   }
 }
